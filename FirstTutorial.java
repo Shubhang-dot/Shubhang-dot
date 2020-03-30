e0009856846c403b50a53bbe6b3873e1e5f14179
Shubhang-dot <55317255+Shubhang-dot@users.noreply.github.com>
2020-03-30 12:33:51 +0530

    Add files via upload

---
+++ FirstTutorial.java
@@ -0,0 +1,7 @@
+package Tutorials;
+
+public class FirstTutorial {
+    public static void main(String[]args){
+       System.out.println("Hello,My name is Shubhang");
+    }
+}
---
+++ SecondTutorial.java
@@ -0,0 +1,9 @@
+package Tutorials;
+
+public class SecondTutorial {
+    public static void main(String[] args) {
+        int f=10,s =5,t=30,fo=40,mixed;
+        mixed = f+s/t*fo%f;
+        System.out.println(mixed);
+    }
+}
