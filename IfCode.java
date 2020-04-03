087c0f4cf8bb011d0bc3497de94826768b437c1a
Shubhang-dot <55317255+Shubhang-dot@users.noreply.github.com>
2020-04-03 21:30:30 +0530

    part -1
    This is part 1 of the if loop tutorial

---
+++ IfCode.java
@@ -0,0 +1,11 @@
+package Tutorials;
+
+public class IfCode {
+    public static void main(String[] args) {
+        if (10>2){
+            System.out.print("Condition satisfied");
+        }else {
+            System.out.println("Reverse is possible ");
+        }
+    }
+}
