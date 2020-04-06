74696f6ec2d723ab23e024b46d856697ab9d43f7
Shubhang-dot <55317255+Shubhang-dot@users.noreply.github.com>
2020-04-06 19:24:37 +0530

    Add files via upload
    code of Tutorial #4

---
+++ ForCode.java
@@ -0,0 +1,9 @@
+package Tutorials;
+
+public class ForCode {
+    public static void main(String[] args) {
+        for(int i = 1; i<=5; i++){
+            System.out.println("yes");
+        }
+    }
+}
